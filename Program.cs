d9e6bf94e4c81aab52d1c825a923f5e592cea916
hicode002 <105335451+hicode002@users.noreply.github.com>
2025-11-18 19:04:14 +0800

    Add intial program

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Potato.ImageFlasher;
+//using Potato.Fastboot;
+using System.Security.Cryptography;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualBasic;
+using System.ComponentModel;
+using System.Runtime.Intrinsics.Arm;
+using System.Collections;
+using System.Diagnostics;
+using System.Reflection.Emit;
+using System.Drawing.Printing;
+using Microsoft.Win32;
+namespace kirintool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirintool Program");
+        }
+    }
+}
